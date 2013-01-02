4975c5ce860c79eed49faab29c3efb5ca8d17647
Matthew Gerring <gerring.matthew@gmail.com>
2013-01-02 00:13:50 -0600

    fixing asset compilation problems on Heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Resumatic
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Required to avoid asset compilation failure on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
