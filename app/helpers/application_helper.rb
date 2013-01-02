75327c1f30f2815b002455aa37883cacb29a7512
Matthew Gerring <gerring.matthew@gmail.com>
2013-01-02 02:42:16 -0600

    marginally better OGP badge, live.js in development only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 	def asset_url(source)
 		return "#{request.protocol}#{request.host_with_port}#{asset_path(source)}"
 	end
+	
+	def yield_or(name, content = nil, &block)
+		if content_for?(name)
+			content_for(name)
+		else
+			block_given? ? capture(&block) : content
+		end
+	end
 end
